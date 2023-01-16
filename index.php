6218f40fe810e20a7d45a3fa6ea9fce0926c0d02
riccardopettoello <113585631+riccardopettoello@users.noreply.github.com>
2023-01-16 17:49:43 +0100

    aggiunta parte raccolta preferenza

--- index.php
+++ index.php
@@ -1,5 +1,8 @@
 <?php
 
+$json_file = file_get_contents('dolceSalato.json');
+$scelte = json_decode($json_file, true);
+
 session_start();
 
 if(!isset($_SESSION['auth'])){
@@ -7,10 +10,10 @@ if(!isset($_SESSION['auth'])){
 }
 
 ?>
-
+<!DOCTYPE html>
 <html lang="it">
 <head>
-    <title>Secure Login</title>
+    <title>Dolce o Salato?</title>
 </head>
 <body>
     <h1>INDEX</h1>
@@ -20,14 +23,22 @@ if(!isset($_SESSION['auth'])){
         il cibo dolce o salato.
         <b>Esprimi la tua opinione!</b></p>
 
-    <form action="">
-        <label for="scelta">Scelta: </label>
-        <label>
-            <select name="Dolce o salato">
-                <option value="dolce">Dolce</option>
-                <option value="salato">Salato</option>
-            </select>
-        </label>
+    <form action="AddScelta.php" method="post">
+        <div>
+            <label for="nome">Nome</label>
+            <input type="text" name="nome" id="nome" required placeholder="Mario">
+        </div>
+        <div>
+            <label for="cognome">Cognome</label>
+            <input type="text" name="cognome" id="cognome" required placeholder="Rossi">
+        </div>
+        <label for="scelta">Inserisci la tua preferenza:</label>
+        <select name="scelta" id="scelta">
+            <?php foreach ($scelte as $scelta): ?>
+            <option value="<?php echo $scelta['scelta'] ?>"><?php echo $scelta['scelta'] ?></option>
+            <?php endforeach; ?>
+        </select>
+        <input type="submit" value="Invia Preferenza">
     </form>
 
     <a href="logout.php" >LOGOUT</a>
