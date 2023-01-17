a4fafd37c8ae55bfb2a7f4e98fe1feeedf027dc9
riccardopettoello <113585631+riccardopettoello@users.noreply.github.com>
2023-01-17 15:24:06 +0100

    aggiunta parte raccolta nome e cognome

--- index.php
+++ index.php
@@ -1,12 +1,33 @@
 <?php
-
-$json_file = file_get_contents('dolceSalato.json');
-$scelte = json_decode($json_file, true);
-
 session_start();
 
+//funzione controllo dati inseriti
+function checkData($temp) {
+    $temp = trim($temp); //toglie spazi all'inizio e alla fine
+    $temp = stripcslashes($temp); //rimuove caratteri speciali che potrebbero generare problemi nel DB
+    $temp = htmlspecialchars($temp); //rimuove tag html per evitare la creazione di problemi
+    return $temp;
+}
+
+$nameErr = $cognomeErr = "";
+$name = $cognome = "";
+
+if($_SERVER["REQUEST_METHOD"] == "POST") {
+    if (empty($_POST["name"])) {             //controllo la presenza dei dati
+        $nameErr = "Campo mancante";
+    } else {
+        $name = checkData($_POST["name"]);
+    }
+
+    if (empty($_POST["cognome"])) {
+        $cognomeErr = "Campo mancante";
+    } else {
+        $cognome = checkData($_POST["name"]);
+    }
+}
+
 if(!isset($_SESSION['auth'])){
-    header("Location:login.php");
+    header("Location: login.php");
 }
 
 ?>
@@ -14,9 +35,15 @@ if(!isset($_SESSION['auth'])){
 <html lang="it">
 <head>
     <title>Dolce o Salato?</title>
+
+    <style>
+        .errore {
+            color: red;
+        }
+    </style>
 </head>
 <body>
-    <h1>INDEX</h1>
+    <h1>Dolce o salato?</h1>
 
     <p>Benvenuto!</p>
     <p>Stiamo raccogliendo sulle preferenze delle persone, riguardo
@@ -24,11 +51,18 @@ if(!isset($_SESSION['auth'])){
         <b>Esprimi la tua opinione!</b></p>
 
     <form action="AddScelta.php" method="post">
+        <div>
+            <label>Nome </label>
+            <input type="text" name="name" value="<?php echo $name ?>"> <span class="errore"> <?php echo $nameErr ?> </span>
+        </div>
+        <div>
+            <label>Cognome </label>
+            <input type="text" name="cognome" value="<?php echo $cognome ?>"> <span class="errore"> <?php echo $cognomeErr ?> </span>
+        </div>
         <label for="scelta">Inserisci la tua preferenza:</label>
         <select name="scelta" id="scelta">
-            <?php foreach ($scelte as $scelta): ?>
-            <option value="<?php echo $scelta['scelta'] ?>"><?php echo $scelta['scelta'] ?></option>
-            <?php endforeach; ?>
+            <option value="dolce" name="dolce" id="dolce">Dolce</option>
+            <option value="salato" name="salato" id="salato">Salato</option>
         </select>
         <input type="submit" value="Invia Preferenza">
     </form>
