b203d285b36c675f21ee6f04b639946aaca23779
mainardis <113585611+mainardis@users.noreply.github.com>
2023-01-17 16:29:30 +0100

    Update index.php

--- index.php
+++ index.php
@@ -22,7 +22,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
     if (empty($_POST["cognome"])) {
         $cognomeErr = "Campo mancante";
     } else {
-        $cognome = checkData($_POST["name"]);
+        $cognome = checkData($_POST["cognome"]);
     }
 }
 
@@ -30,6 +30,10 @@ if(!isset($_SESSION['auth'])){
     header("Location: login.php");
 }
 
+if($name != "" && $cognome != "") {
+    $_SESSION = $_POST;
+    header("Location: pagina2.php");
+}
 ?>
 <!DOCTYPE html>
 <html lang="it">
@@ -37,13 +41,53 @@ if(!isset($_SESSION['auth'])){
     <title>Dolce o Salato?</title>
 
     <style>
+
+        .title{
+            text-align: center;
+            font-style: italic;
+        }
+
+        p{
+            text-indent: 30px;
+        }
+
+        input[type=text], select {
+            width: 100%;
+            padding: 12px 20px;
+            margin: 8px 0;
+            display: inline-block;
+            border: 1px solid #ccc;
+            border-radius: 4px;
+            box-sizing: border-box;
+        }
+
+        input[type=submit] {
+            width: 100%;
+            background-color: #FF0000;
+            color: white;
+            padding: 14px 20px;
+            margin: 8px 0;
+            border: none;
+            border-radius: 4px;
+            cursor: pointer;
+        }
+
+        input[type=submit]:hover {
+            background-color: #000000;
+        }
+
+        div {
+            border-radius: 5px;
+            background-color: #f2f2f2;
+            padding: 20px;
+        }
         .errore {
             color: red;
         }
     </style>
 </head>
 <body>
-    <h1>Dolce o salato?</h1>
+    <h1 class="title">Dolce o salato?</h1>
 
     <p>Benvenuto!</p>
     <p>Stiamo raccogliendo sulle preferenze delle persone, riguardo
